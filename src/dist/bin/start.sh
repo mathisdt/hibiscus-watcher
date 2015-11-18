01e2b97ca5e1b4b06fbca2ac26117289a064300d
Mathis Dirksen-Thedens <dev@zephyrsoft.org>
2015-11-18 19:55:22 +0000

    start script cleanup
    git-svn-id: file:///home/svnroot-zephyrsoft/projects/hibiscus-watcher/trunk@660 f37aa4ac-6963-46a2-a1ff-558a307fa857

--- src/dist/bin/start.sh
+++ src/dist/bin/start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 BIN_DIR=$(dirname $(readlink -f "$0"))
-BASE_DIR=$(readlink -f "$BIN_DIR/..")
-java -Duser.language=de -Duser.country=DE -Dbase.dir="$BASE_DIR" -jar "$BIN_DIR/hibiscus-watcher.jar" $*
+java -Duser.language=de -Duser.country=DE -jar "$BIN_DIR/hibiscus-watcher.jar" $*
